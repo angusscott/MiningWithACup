b11aea51d9e1488bd6724c9b45d74abb6cb50de1
David Selby <david@selbys.org.uk>
2014-06-12 00:29:54 +0100

    Mexican odds. Ability to label points by country

--- analysis.R
+++ analysis.R
@@ -12,13 +12,22 @@ dataset$interest <- sapply(tweets, function(x) x/sum(tweets))
 attach(dataset)
 #sum(interest) # = 1
 
+#labels
+#if I can be bothered I'll set this up for every country
+countries.of.interest <- c("england","australia","usa","brazil")
+codes <- c("en","au","us","br")
+labels=rep("",length(country))
+labels[match(countries.of.interest,country)]=codes 
+
 #Correlation (absolute)
 cor.test(odds, tweets) #gives a PMCC of 0.6-0.7 depending on whether you use tweets or log(tweets)
-plot(odds, log(tweets), main="World Cup competitors", xlab="Paddy Power odds", ylab="log(# of tweets)")
-text(odds, log(tweets), labels=country)
+plot(log(odds), log(tweets), main="World Cup competitors", xlab="log(odds)", ylab="log(# of tweets)")
+text(log(odds), log(tweets), labels, pos=1, offset=0.2)
 
 #Simple linear regression
-model <- lm(log(tweets)~odds)
+model <- lm(log(tweets)~log(odds))
+abline(model,col="red")
+model.nolog <- lm(tweets~odds)
 summary(model)
 
 #Diagnostic plots (residuals and QQ) to test normality
